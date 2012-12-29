28fee2699158b2713d41056a05c467e1cf4ca063
Xifax <demi.log@gmail.com>
2012-12-29 15:19:54 +0400

    Scafforlding everything for new implementation

--- src/hotkeys.py
+++ src/hotkeys.py
@@ -11,24 +11,24 @@ class GlobalHotkeyManager(Thread):
         Thread.__init__(self)
         self.function = function
         self.key = key
-        
+
         self.byref = ctypes.byref
         self.user32 = ctypes.windll.user32
-        
+
         self.event = Event()
 
         #TODO: add configurable modifiers & multiple hotkeys
-        
+
         self.id = 1
         self.modifiers = win32con.MOD_CONTROL + win32con.MOD_ALT
         self.vk = ord(self.key)
-    
-    def registerHotkey(self):        
+
+    def registerHotkey(self):
         if not self.user32.RegisterHotKey(None, self.id, self.modifiers, self.vk):
             print 'Unable to register id', id, '\n'
         else:
             print 'registered ' + str(self.id) + ' ' + self.key + '\n'
-        
+
     def messageLoop(self):
         try:
             msg = wintypes.MSG()
@@ -36,17 +36,17 @@ class GlobalHotkeyManager(Thread):
             while self.user32.GetMessageA (self.byref (msg), None, 0, 0) != 0:
                 if msg.message == win32con.WM_HOTKEY:
                     self.function()
-        
+
             self.user32.TranslateMessage (self.byref (msg))
             self.user32.DispatchMessageA (self.byref (msg))
-            
+
         finally:
             self.unregisterHotkeys()
             print 'Hotkey unregistered\n'
-    
+
     def unregisterHotkeys(self):
         self.user32.UnregisterHotKey(None, self.id)
-        
+
     def run(self):
         self.registerHotkey()
         self.messageLoop()
